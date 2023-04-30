3cdaa900a67ee9c7e8d7182ebb96bb5f41b47b32
Super-WeiDong <weidongwang@brandeis.edu>
2023-04-30 17:23:31 -0400

    membership_model

---
+++ model/membership.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const membershipSchema = mongoose.Schema(
+  {
+  type: { type: String, required: true },
+  duration: { type: Number, required: true},
+  price: { type: Number,required: true},
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Membership", membershipSchema);

6eccd991a957c4e6025318ce8100e315c375c26d
Super-WeiDong <weidongwang@brandeis.edu>
2023-04-30 21:38:10 -0400

    membership_routes

--- routes/membershipRoutes.js
+++ routes/membershipRoutes.js
@@ -1,1 +1,30 @@
 //Weidong
+const router = require("express").Router();
+const membershipsController = require("../controllers/membershipsController");
+const usersController = require("../controllers/usersController");
+
+//jobs Routes
+router.get("/",usersController.isLoggedIn,membershipsController.index,membershipsController.indexView);
+router.get("/new", usersController.isLoggedIn,membershipsController.new);
+router.post(
+  "/create",
+  usersController.isLoggedIn,
+  membershipsController.create,
+  membershipsController.redirectView
+);
+router.get("/:id", usersController.isLoggedIn, membershipsController.show, membershipsController.showView);
+router.get("/:id/edit", usersController.isLoggedIn, membershipsController.edit);
+router.put(
+  "/:id/update",
+  usersController.isLoggedIn,
+  membershipsController.update,
+  membershipsController.redirectView
+);
+router.delete(
+  "/:id/delete",
+  usersController.isLoggedIn,
+  membershipsController.delete,
+  membershipsController.redirectView
+);
+
+module.exports = router;

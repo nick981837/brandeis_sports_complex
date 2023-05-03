8263e0002202a1903804f69353a92f0f3a42e121
Alex Romer <aljoromer@gmail.com>
2023-05-02 21:44:38 -0400

    eslint cleanup

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
-const User = require("../model/user");
-const passport = require("passport");
+const User = require('../model/user');
+const passport = require('passport');
 
 const getUserParams = (body) => {
   return {
@@ -15,23 +15,23 @@ module.exports = {
   // only available to admins
   index: (req, res, next) => {
     User.find()
-      .then((users) => {
-        res.locals.users = users;
-        next();
-      })
-      .catch((error) => {
-        console.log(`Error fetching users: ${error.message}`);
-        next(error);
-      });
+        .then((users) => {
+          res.locals.users = users;
+          next();
+        })
+        .catch((error) => {
+          console.log(`Error fetching users: ${error.message}`);
+          next(error);
+        });
   },
 
   indexView: (req, res) => {
-    res.render("users/index");
+    res.render('users/index');
   },
 
   // get /users/new
   new: (req, res) => {
-    res.render("users/new");
+    res.render('users/new');
   },
   // post //users/new
   create: (req, res, next) => {
@@ -39,15 +39,15 @@ module.exports = {
     const newUser = new User(getUserParams(req.body));
     User.register(newUser, req.body.password, (error, user) => {
       if (user) {
-        req.flash("success", `${user.name}'s account created successfully!`);
-        res.locals.redirect = "/";
+        req.flash('success', `${user.name}'s account created successfully!`);
+        res.locals.redirect = '/';
         next();
       } else {
         req.flash(
-          "error",
-          `Failed to create user account because: ${error.message}.`
+            'error',
+            `Failed to create user account because: ${error.message}.`,
         );
-        res.locals.redirect = "/users/new";
+        res.locals.redirect = '/users/new';
         next();
       }
     });
@@ -64,35 +64,35 @@ module.exports = {
   show: (req, res, next) => {
     const userId = req.params.id;
     User.findById(userId)
-      .populate("memberships")
-      .then((user) => {
-        res.locals.user = user;
-        next();
-      })
-      .catch((error) => {
-        console.log(`Error fetching user by ID: ${error.message}`);
-        next(error);
-      });
+        .populate('memberships')
+        .then((user) => {
+          res.locals.user = user;
+          next();
+        })
+        .catch((error) => {
+          console.log(`Error fetching user by ID: ${error.message}`);
+          next(error);
+        });
   },
 
   showView: (req, res) => {
     const user = res.locals.user; // Get user object from res.locals
-    res.render("users/show", { user });
+    res.render('users/show', {user});
   },
 
   // get /users/:id/edit
   edit: (req, res, next) => {
     const userId = req.params.id;
     User.findById(userId)
-      .then((user) => {
-        res.render("users/edit", {
-          user: user,
+        .then((user) => {
+          res.render('users/edit', {
+            user: user,
+          });
+        })
+        .catch((error) => {
+          console.log(`Error fetching user by ID: ${error.message}`);
+          next(error);
         });
-      })
-      .catch((error) => {
-        console.log(`Error fetching user by ID: ${error.message}`);
-        next(error);
-      });
   },
 
   // put /users/:id/update
@@ -107,71 +107,71 @@ module.exports = {
     User.findByIdAndUpdate(userId, {
       $set: userParams,
     })
-      .then((user) => {
-        req.flash("success", "User successfully updated");
-        res.locals.redirect = `/users/${userId}`;
-        res.locals.user = user;
-        next();
-      })
-      .catch((error) => {
-        console.log(`Error updating user by ID: ${error.message}`);
-        next(error);
-      });
+        .then((user) => {
+          req.flash('success', 'User successfully updated');
+          res.locals.redirect = `/users/${userId}`;
+          res.locals.user = user;
+          next();
+        })
+        .catch((error) => {
+          console.log(`Error updating user by ID: ${error.message}`);
+          next(error);
+        });
   },
 
   // delete /users/:id/delete
   delete: (req, res, next) => {
     const userId = req.params.id;
     User.findByIdAndRemove(userId)
-      .then(() => {
-        req.flash("success", "User successfully deleted");
-        res.locals.redirect = "/users";
-        next();
-      })
-      .catch((error) => {
-        console.log(`Error deleting user by ID: ${error.message}`);
-        next();
-      });
+        .then(() => {
+          req.flash('success', 'User successfully deleted');
+          res.locals.redirect = '/users';
+          next();
+        })
+        .catch((error) => {
+          console.log(`Error deleting user by ID: ${error.message}`);
+          next();
+        });
   },
 
   // get /users/login
   login: (req, res) => {
-    res.render("users/login");
+    res.render('users/login');
   },
 
   // get /users/logout
   logout: (req, res, next) => {
-    req.logout(function (err) {
+    req.logout(function(err) {
       if (err) {
         return next(err);
       }
-      req.flash("success", "You have been logged out!");
-      res.locals.redirect = "/";
+      req.flash('success', 'You have been logged out!');
+      res.locals.redirect = '/';
       next();
     });
   },
 
   // Authenticates with passport
   // post /users/login
-  authenticate: passport.authenticate("local", {
-    failureRedirect: "/users/login",
-    failureFlash: "Failed to login.",
-    successRedirect: "/",
+  authenticate: passport.authenticate('local', {
+    failureRedirect: '/users/login',
+    failureFlash: 'Failed to login.',
+    successRedirect: '/',
   }),
 
   validate: (req, res, next) => {
-    req.sanitizeBody("email").trim();
-    req.sanitizeBody("email").normalizeEmail({
+    req.sanitizeBody('email').trim();
+    req.sanitizeBody('email').normalizeEmail({
       all_lowercase: true,
     });
-    req.check("email", "Email is invalid").isEmail();
-    req.check("password", "Password cannot be empty").notEmpty();
+    req.check('email', 'Email is invalid').isEmail();
+    req.check('password', 'Password cannot be empty').notEmpty();
     req.getValidationResult().then((error) => {
       if (!error.isEmpty()) {
         const messages = error.array().map((e) => e.msg);
         req.skip = true;
-        req.flash("error", messages.join(" and "));
-        res.locals.redirect = "/users/new";
+        req.flash('error', messages.join(' and '));
+        res.locals.redirect = '/users/new';
         next();
       } else {
         next();
@@ -184,8 +184,8 @@ module.exports = {
     if (req.isAuthenticated()) {
       next();
     } else {
-      req.flash("error", "Please sign in.");
-      res.redirect("/users/login");
+      req.flash('error', 'Please sign in.');
+      res.redirect('/users/login');
     }
   },
 
@@ -195,12 +195,12 @@ module.exports = {
       if (req.user.isAdmin) {
         next();
       } else {
-        req.flash("error", "You are not an admin.");
-        res.redirect("/");
+        req.flash('error', 'You are not an admin.');
+        res.redirect('/');
       }
     } else {
-      req.flash("error", "Please sign in.");
-      res.redirect("/users/login");
+      req.flash('error', 'Please sign in.');
+      res.redirect('/users/login');
     }
   },
 
@@ -212,12 +212,12 @@ module.exports = {
       } else if (req.user.isAdmin) {
         next();
       } else {
-        req.flash("error", "You are not authorized to perform this action.");
-        res.redirect("/");
+        req.flash('error', 'You are not authorized to perform this action.');
+        res.redirect('/');
       }
     } else {
-      req.flash("error", "Please sign in.");
-      res.redirect("/users/login");
+      req.flash('error', 'Please sign in.');
+      res.redirect('/users/login');
     }
   },
 };
